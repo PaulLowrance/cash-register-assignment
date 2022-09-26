ffd0daefcf371738a54f0dc030b1bb910ec1a8cf
Paul Lowrance <tpaul.lowrance@gmail.com>
2022-09-26 01:51:10 -0500

    Added Unit tests and working CLI.

---
+++ CASHMasters.RegisterLib.Tests/CashRegisterServiceTests.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using CASHMasters.RegisterLib.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CASHMasters.RegisterLib.Tests;
+
+[TestClass]
+public class RegisterService_MinimumNumberOfCoins
+{
+    private IOptions<CashRegisterOptions> _registerSettings;
+    private Mock<ILogger<ICashRegisterService>> _loggerMock;
+    private Mock<IDictionary<string, decimal[]>> _configuredDenominationsMock;
+
+    [TestInitialize]
+    public void InitializeMocks()
+    {
+        _loggerMock = new Mock<ILogger<ICashRegisterService>>();
+        _configuredDenominationsMock = new Mock<IDictionary<string, decimal[]>>();
+
+        var mockDenomOptions = new DenominationOptions
+        {
+            CurrentDenomination = "USA",
+            ConfiguredDenominations = new Dictionary<string, decimal[]>()
+        };
+        mockDenomOptions.ConfiguredDenominations.Add("USA",
+            new[] {0.01m, 0.05m, 0.10m, 0.25m, 0.50m, 1.00m, 2.00m, 5.00m, 10.00m, 20.00m, 50.00m, 100.00m });
+        mockDenomOptions.ConfiguredDenominations.Add("Mexico",
+            new[] { 0.05m, 0.10m, 0.25m, 0.50m, 1.00m, 2.00m, 5.00m, 10.00m, 20.00m, 50.00m, 100.00m });
+
+        _registerSettings = new OptionsWrapper<CashRegisterOptions>(new CashRegisterOptions()
+            { DenominationOptions = mockDenomOptions });
+    }
+
+    [TestMethod]
+    public void HandleExactChange()
+    {
+        var service = new CashRegisterService(_loggerMock.Object, _registerSettings);
+
+        var result = service.GetChangeWithLeastNumberOfCoins(5.15m, new[] { 5.00m, 0.10m, 0.05m }).Result;
+
+        Assert.AreEqual(0, result);
+    }
+
+
+    [TestMethod]
+    [ExpectedException(typeof(AggregateException),
+        noExceptionMessage: "An exception is expected due to remaining payment needed")]
+    public void NotEnoughPayment()
+    {
+        var service = new CashRegisterService(_loggerMock.Object, _registerSettings);
+
+        var result = service.GetChangeWithLeastNumberOfCoins(5.15m, new[] { 5.00m, 0.05m }).Result;
+    }
+    
+    [TestMethod]
+    public void ExampleCase1FromAssignment()
+    {
+        var service = new CashRegisterService(_loggerMock.Object, _registerSettings);
+
+        var result = service.GetChangeWithLeastNumberOfCoins(5.25m, new[] { 5.00m, 1.00m }).Result;
+
+        //expecting a .50 and .25 piece
+        Assert.AreEqual(2, result);
+    }
+    
+    [TestMethod]
+    public void ExampleCase2FromAssignment()
+    {
+        var service = new CashRegisterService(_loggerMock.Object, _registerSettings);
+
+        var result = service.GetChangeWithLeastNumberOfCoins(5.25m, new[] { 5.00m, 0.10m , 0.10m, 0.10m, 0.10m, 0.10m, 0.10m, 0.10m, 0.10m, 0.10m, 0.10m}).Result;
+
+        //expecting a .50 and .25 piece
+        Assert.AreEqual(2, result);
+    }
+    
+    [TestMethod]
+    public void ExampleCase_UseingPennies()
+    {
+        var service = new CashRegisterService(_loggerMock.Object, _registerSettings);
+
+        var result = service.GetChangeWithLeastNumberOfCoins(5.03m, new[] { 5.00m, 0.10m}).Result;
+
+        //expecting a .05 and 2 .01 pieces
+        Assert.AreEqual(3, result);
+    }
+}
--- CASHMastersRegister/Program.cs
+++ CASHMastersRegister/Program.cs
@@ -4,30 +4,100 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-static void ConfigureServices(IServiceCollection services)
+namespace CASHMastersRegister;
+
+public static class Program
 {
-    services.AddLogging(bldr =>
+    static async Task Main(string[] args)
     {
-        bldr.AddConsole();
-        bldr.AddDebug();
-    });
+        var services = new ServiceCollection();
+        ConfigureServices(services);
 
-    var configuration = new ConfigurationBuilder().
-        SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("appsettings.json", optional: false)
-        .AddEnvironmentVariables()
-        .Build();
+        await using var serviceProvider = services.BuildServiceProvider();
 
-    services.Configure<CashRegisterOptions>(configuration.GetSection("CashRegisterConfiguration"));
-    services.AddTransient<ICashRegisterService, CashRegisterService>();
-    
+        var validationResult = ValidateInputs(args);
+
+        if (!validationResult.isValid)
+        {
+            PrintHelpText();
+            return;
+        }
+
+        var result = await serviceProvider.GetService<ICashRegisterService>()
+            .GetChangeWithLeastNumberOfCoins(validationResult.totalCharges, validationResult.providedCoins);
+
+        Console.WriteLine($"Result: {result}");
+    }
+
+    static (bool isValid, decimal totalCharges, decimal[] providedCoins) ValidateInputs(string[] args)
+    {
+        if (args.Any(arg => arg.ToLower().Equals("--help")))
+        {
+            return (false, decimal.MinValue, Array.Empty<decimal>());
+        }
+
+        if (args.Contains("--purchase-amount") && args.Contains("--customer-payment"))
+        {
+            var argList = args.ToList();
+            if (!decimal.TryParse(argList[argList.IndexOf("--purchase-amount") + 1], out var purchaseAmount))
+            {
+                Console.WriteLine($"Unable to convert Purchase Price to decimal");
+                return (false, Decimal.MinValue, Array.Empty<decimal>());
+            }
+            
+            if (ConvertDenominationArgToArray(argList[argList.IndexOf("--customer-payment") + 1], out decimal[] customerPayment))
+            {
+                return (true, purchaseAmount, customerPayment);
+            }
+        }
+
+        PrintHelpText();
+        return (false, decimal.MinValue, Array.Empty<decimal>());
+    }
+
+    private static void PrintHelpText()
+    {
+        Console.WriteLine("USAGE: CASHMastersRegister --purchase-amount <total cost of items> --customer-payment <comma-separated list of bills/coins from the customer>");
+        Console.WriteLine("EXAMPLE: CASHMastersRegister --purchase-amount 5.25 --customer-payment \"5.00, 1.00\"");
+    }
+
+    private static bool ConvertDenominationArgToArray(string customerPaymentString, out decimal[] denominationArray)
+    {
+        var splitString =
+            customerPaymentString.Split(',', StringSplitOptions.RemoveEmptyEntries);
+        var bills = new List<decimal>();
+        foreach (var item in splitString)
+        {
+            if (decimal.TryParse(item, out var decimalItem))
+            {
+                bills.Add(decimalItem);
+            }
+            else
+            {
+                Console.WriteLine($"Provided value {item}, is not a decimal number");
+                denominationArray = Array.Empty<decimal>();
+                return false;
+            }
+        }
+
+        denominationArray = bills.ToArray();
+        return true;
+    }
+
+    static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddLogging(bldr =>
+        {
+            bldr.AddConsole();
+            bldr.AddDebug();
+        });
+
+        var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddEnvironmentVariables()
+            .Build();
+
+        services.Configure<CashRegisterOptions>(configuration.GetSection("CashRegisterConfiguration"));
+        services.AddTransient<ICashRegisterService, CashRegisterService>();
+    }
 }
-
-var services = new ServiceCollection();
-ConfigureServices(services);
-
-await using var serviceProvider = services.BuildServiceProvider();
-
-var result = await serviceProvider.GetService<ICashRegisterService>().GetChangeWithLeastNumberOfCoins(5.15m, new decimal[]{5.00m, 1.00m});
-
-Console.WriteLine($"Result: {result}");
